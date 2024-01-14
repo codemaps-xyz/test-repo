eda43ed8066299157f127ed3c6b8367d711273be
Soumik Dasgupta <soumikdasgupta@Soumiks-MacBook-Air.local>
2024-01-15 00:25:35 +0530

    bugfix

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import { Container, Flex, Heading, Text, Grid } from '@radix-ui/themes'
 // import Navbar from '@/components/NavBar'
 import styles from '@/styles/Home.module.css'
 import { headers } from '@/next.config'
-//import { headers } from 'next/headers'
-import { cookies } from 'next/headers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,9 +24,9 @@ export async function getStaticProps(context) {
   try {
     console.log('CONTEXT : ',context)
     console.log("getStaticProps started");
-    const cookieStore = cookies()
-    const cookieString = cookieStore.getAll()
-    // const cookies = context.req.headers.cookie;
+    // const cookieStore = cookies()
+    // const cookieString = cookieStore.getAll()
+    const cookies = context.req.headers.cookie;
     console.log( cookieString )
 
     const baseUrl = 'https://test-repo-1py6vevk2-codemaps-projects.vercel.app';
@@ -36,8 +34,8 @@ export async function getStaticProps(context) {
 
     const apiEndpoint = `${baseUrl}/api/test`;
     console.log("Fetching data from:", apiEndpoint);
-
-    const res = await fetch(apiEndpoint, { headers: { Cookie : '_vercel_jwt=' + 'cookies' } } );
+    //const options = { headers: { Cookie : '_vercel_jwt=' + 'cookies' } };
+    const res = await fetch(apiEndpoint);
 
     if (!res.ok) {
       console.error("Failed to fetch data. Status:", res.status);
